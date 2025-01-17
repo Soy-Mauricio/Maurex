7c96574fcfb25251448e16eac25b09a70676ca53
Soy-Mauricio <maurosana6@gmail.com>
2025-01-16 22:13:22 -0500

    Creacion de vista Web 1.3

--- src/pages/Web.tsx
+++ src/pages/Web.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import MainLayout from '../layouts/MainLayout';
 import ServicesWeb from '../components/webSections/ServicesWeb';
+import Reviews from '../components/ourWorkSections/Reviews';
+import Trajectory from '../components/ourWorkSections/Trajectory';
 
 const Web: React.FC = () => {
     return (
@@ -81,13 +83,13 @@ const Web: React.FC = () => {
             </div>
         </section>
 
-        <section className="auto mt-10">
-            <div className="h-[auto] p-4 max-w-[1200px] w-full mx-auto">
-                <div className="sm:mx-8">
-                    <div className="flex justify-start">
-                        <div className="w-[60%]">
-                            <h2 className="text-with-icon text-3xl opacity-80 font-bold">Desarrollo Web de Calidad</h2>
-                            <div className="mt-4 text-2xl opacity-80 sm:w-[85%]">
+        <section className="auto">
+            <div className="h-[auto] p-4 max-w-[1200px] w-full mx-auto  mt-20">
+                <div className="sm:mx-8 flex">
+                    <div className="flex justify-start w-[60%]">
+                        <div className="w-[90%]">
+                            <h2 className="icon text-5xl opacity-80 font-bold">Desarrollo Web de Calidad</h2>
+                            <div className="mt-12 text-2xl opacity-80 ">
                                 <p className="mb-4">En Maurex, abordamos el desarrollo web con un enfoque claro y eficiente. Te acompañamos en cada paso, desde la planificación hasta el lanzamiento. Analizamos tu proyecto, investigamos el mercado y te asesoramos sobre la tecnología adecuada, la arquitectura y las mejoras necesarias para mantener tu producto competitivo.
                                 </p>
                                 <p>
@@ -96,6 +98,75 @@ const Web: React.FC = () => {
                             </div>
                         </div>
                     </div>
+
+                    <div className="ml-10 w-[40%] flex items-center">
+                        <img className="object-cover h-[600px]" src="/public/assets/Img/web-design.webp" alt="" />
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+        <section className="h-auto mt-24 color-sections-dos">
+            {/* Contenido de Bienvenida */}
+            <div className="max-w-[1200px] w-full mx-auto h-auto flex items-center">
+                <div className="h-auto mb-4 w-full">
+                    <div className="mt-12 p-4 sm:mx-8 text-white flex flex-col sm:flex-row justify-between items-center">
+                        <div className="">
+                            <h2 className="icon-two text-4xl font-bold mb-10">Testimonios de clientes</h2>
+                            <span className="text-xl italic">Tenemos más de 10+ reseñas con una puntuación media de</span>
+                        </div>
+
+                        <div className="mt-8 sm:mt-0 flex flex-col  justify-around items-center">
+                            <div className="puntuacion sm:text-9xl text-7xl font-extrabold">5.0</div>
+                            <div className="text-3xl">★ ★ ★ ★ ★</div>
+                        </div>
+                    </div>
+
+                    <div className="p-4 sm:mx-8">
+                        <div className="flex justify-center mt-14 mb-12">
+                            <Reviews/>
+                        </div>
+
+                        <div className="mt-20 mb-10 text-xl italic text-white">
+                            <Trajectory 
+                            projects='+10 proyectos completados.'
+                            hours='+500 horas de desarrollo acumuladas.'
+                            satisfaction='100% de satisfacción en entregas puntuales.'
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section className="auto">
+            <div className="h-[auto] p-4 max-w-[1200px] w-full mx-auto mt-20">
+                <div className="sm:mx-8 flex flex-col justify-center">
+                    <div className="flex justify-center">
+                        <div className="w-[80%] flex flex-col">
+                            <h2 className="icon-three text-5xl opacity-80 font-bold text-center">Una muestra de nuestro portafolio de desarrollo web</h2>
+                            <div className="mt-12 text-2xl opacity-80">
+                                <p className="mb-4 text-center">
+                                Desde la conceptualización hasta el lanzamiento, seguimos un proceso ágil para asegurar que cada proyecto se complete a tiempo y dentro del presupuesto.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="flex flex-col">
+                        <div className="mt-[150px]">
+                            <h3 className="text-4xl font-semibold">Agencia de Vuelos</h3>
+                        </div>
+                        <div className="mt-10">
+                            <p className="text-2xl opacity-80">VueloYA es una innovadora aplicación diseñada para transformar la manera en que las personas buscan, reservan y gestionan sus vuelos. Con un enfoque en la simplicidad y la eficiencia, VueloYA se destaca en un mercado saturado por plataformas poco intuitivas, ofreciendo una experiencia de usuario clara y amigable.</p>
+                        </div>
+
+                        <div className="flex justify-center mt-10">
+                            <img src="/public/assets/Img/WebVueloYA.webp" alt="" />
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </section>
